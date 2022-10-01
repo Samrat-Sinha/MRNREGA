87a34446ca397ea911c45b7f1802749cef8df39e
samrat <smrtsnh1995@gmail.com>
2022-10-02 03:22:52 +0530

    Project day-4

--- MRNREGA/src/com/masai/deo/BDO_Deo.java
+++ MRNREGA/src/com/masai/deo/BDO_Deo.java
@@ -1,10 +1,23 @@
 package com.masai.deo;
 
+
+import java.util.List;
+
 import com.masai.beam.BDO;
+import com.masai.beam.GPM;
+import com.masai.beam.Project;
 import com.masai.exceptions.BDOException;
 
 public interface BDO_Deo {
 	
 	public BDO loginBDO(String username,String password)throws BDOException;
-
+   
+	public String CreateProject(Project project);
+	
+	public List<Project> getAllProjectDetails()throws BDOException;
+	
+	public String CreateGPM(GPM gpm);
+	
+	public List<GPM> getAllGpmDetails()throws BDOException;
+	
 }
--- MRNREGA/src/com/masai/frontend/MrnregaApp.java
+++ MRNREGA/src/com/masai/frontend/MrnregaApp.java
@@ -1,8 +1,11 @@
 package com.masai.frontend;
 
+import java.util.List;
 import java.util.Scanner;
 
 import com.masai.beam.BDO;
+import com.masai.beam.GPM;
+import com.masai.beam.Project;
 import com.masai.deo.BDO_DaoImpl;
 import com.masai.deo.BDO_Deo;
 import com.masai.exceptions.BDOException;
@@ -27,18 +30,111 @@ public class MrnregaApp {
 				
 				System.out.println("Enter password");
 				String pass = sc.next();
-				
-				
-				
+								
 				try {
 					BDO bdo = bdodeo.loginBDO(uname, pass);
 					
 					System.out.println("Login Succesfully......");
+					
+					while(true) {
+						System.out.println("\n1. Create a Project");
+						System.out.println("\n2. View List Of Project.");
+						System.out.println("\n3. Create new Gram Panchayat Member(GPM).");
+						System.out.println("\n4. View all the Gram Panchayat Member(GPM).");
+						System.out.println("\n5. Allocate  Project to Gram Panchayat Member(GPM).");
+						System.out.println("\n6. See List of Employee working on that Project and their wages.");
+						System.out.println("\n7. Exit");
+						int bdon = sc.nextInt();
+						if(bdon==1) {
+						
+							System.out.println("Enter Project Title : ");
+							String prodesc = sc.next();
+							
+							System.out.println("Enter Project Destrict : ");
+							String prodist = sc.next();
+							
+							System.out.println("Enter Project Village : ");
+							String provill = sc.next();
+							
+							Project project = new Project();
+							
+							project.setProjectDesc(prodesc);
+							project.setProjectDist(prodist);
+							project.setProjectVillage(provill);
+							
+							String result = bdodeo.CreateProject(project);
+							
+							System.out.println(result);
+							
+						}
+						if(bdon==2) {
+							
+							try {
+								List<Project> projects = bdodeo.getAllProjectDetails();
+								projects.forEach(s->System.out.println(s));
+							}
+							catch(BDOException bd) {
+								System.out.println(bd.getMessage());
+							}
+							
+						}
+						if(bdon==3) {
+							
+							System.out.println("Enter GPM Name : ");
+							String gpmname = sc.next();
+							
+							System.out.println("Enter GPM Village : ");
+							String gpmvill = sc.next();
+							
+							System.out.println("Enter GPM email : ");
+							String gpmeml = sc.next();
+							
+							System.out.println("Enter GPM Password : ");
+							String gpmpsd = sc.next();
+							
+							
+							GPM gpm = new GPM();
+							
+							gpm.setGpmName(gpmname);
+							gpm.setGpmVillage(gpmvill);
+							gpm.setGpmEmail(gpmeml);
+							gpm.setGpmpassword(pass);
+							
+							String result = bdodeo.CreateGPM(gpm);
+							
+							System.out.println(result);
+							
+						}
+						if(bdon==4) {
+							
+							try {
+								List<GPM> gpms = bdodeo.getAllGpmDetails();
+								gpms.forEach(s->System.out.println(s));
+							}
+							catch(BDOException bd) {
+								System.out.println(bd.getMessage());
+							}
+							
+						}
+						if(bdon==5) {
+							
+						}
+						if(bdon==6) {
+							
+						}
+						if(bdon==7) {
+							break;
+						}
+						
+					}
 					MrnregaApp.selectoption();
 					}
 					catch(BDOException bd) {
 						System.out.println(bd.getMessage());
 					}
+				
+			case 2:
+				
 			}
 		}
 		catch(Exception e) {
