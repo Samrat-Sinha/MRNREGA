0da9a3513b2a80df9249cd3fb1c03cbbe2d6b334
samrat <smrtsnh1995@gmail.com>
2022-11-19 05:01:07 +0530

    Project Updated Usecases Package

---
+++ MRNREGA/src/com/masai/usecases/getAllEmployee.java
@@ -0,0 +1,19 @@
+package com.masai.usecases;
+
+import java.util.List;
+
+import com.masai.Dao.GPMDao;
+import com.masai.Dao.GPMDaoImpl;
+import com.masai.Model.Employee;
+
+public class getAllEmployee {
+
+public static List<Employee> viewAllEmp() {
+		
+		GPMDao dao = new GPMDaoImpl();
+		
+		List<Employee> allemp = dao.getAllEmployee();
+
+		return allemp;	
+	}
+}
