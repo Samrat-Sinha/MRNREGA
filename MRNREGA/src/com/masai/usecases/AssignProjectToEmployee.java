1df0b643d32fa495e3601101e8b73f44e54be8dc
samrat <smrtsnh1995@gmail.com>
2022-11-19 04:56:24 +0530

    Project Updated Usecases Package

---
+++ MRNREGA/src/com/masai/usecases/AssignProjectToEmployee.java
@@ -0,0 +1,52 @@
+package com.masai.usecases;
+
+import java.util.Scanner;
+
+import com.masai.Dao.GPMDao;
+import com.masai.Dao.GPMDaoImpl;
+import com.masai.exceptions.EmployeeException;
+import com.masai.exceptions.ProjectException;
+
+public class AssignProjectToEmployee {
+
+public static String AssignEmp() {
+        
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter the Employee ID ");
+		int eid= sc.nextInt();
+
+		System.out.println("Enter the Employee Name ");
+		String en= sc.next();			
+		
+		System.out.println("Enter the Project ID");
+		int pid= sc.nextInt();
+		
+		System.out.println("Enter the Project Name ");
+		String pn= sc.next();
+		
+		System.out.println("Enter Assign of Days");
+		int days = sc.nextInt();
+		
+		GPMDao dao = new GPMDaoImpl();
+		
+//		try {
+		String result="";
+		try {
+			result = dao.allocateEmployeeInsideProject(eid, en, pid, pn,days);
+		} catch (EmployeeException e) {
+			e.printStackTrace();
+		} catch (ProjectException e) {
+			e.printStackTrace();
+		}
+		
+
+		return result;
+//		}catch(Exception e) {
+//			
+//			System.out.println(e.getMessage());
+//			
+//		}
+	}
+	
+}
