6056ed29e062ed8b141f915d2a83a94a3cc30b98
samrat <smrtsnh1995@gmail.com>
2022-11-19 05:06:58 +0530

    Project Updated Usecases Package

---
+++ MRNREGA/src/com/masai/usecases/RegisterBDO.java
@@ -0,0 +1,35 @@
+package com.masai.usecases;
+
+import java.util.Scanner;
+
+import com.masai.Dao.BDODao;
+import com.masai.Dao.BDODaoImpl;
+import com.masai.Model.BDO;
+
+public class RegisterBDO {
+
+public static String RegisterAsBDO() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter BDO Name ");
+		String bdoname = sc.next();
+		
+		System.out.println("Enter BDO email ");
+		String bdoeml = sc.next();
+		
+		System.out.println("Enter BDO password ");
+		String bdopswd = sc.next();
+		
+		BDODao dao = new BDODaoImpl();
+		BDO bdo = new BDO();
+		
+		bdo.setName(bdoname);
+		bdo.setEmail(bdoeml);
+		bdo.setPassword(bdopswd);
+				
+		String result = dao.registerAsBDO(bdo);
+		return result;
+	}
+	
+}
